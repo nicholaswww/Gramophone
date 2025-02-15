6ce20d1d47951571e9a59c228dab27fe9dae23d8
nift4 <nift4@protonmail.com>
2025-02-15 20:43:40 +0100

    cheap credits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -240,7 +240,7 @@ aboutLibraries {
 
 dependencies {
     implementation(project(":libphonograph:libPhonograph"))
-    val media3Version = "1.6.0-alpha01"
+    val media3Version = "1.6.0-alpha03"
     implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.collection:collection-ktx:1.4.5")
@@ -249,7 +249,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.core:core-splashscreen:1.2.0-alpha02")
     //implementation("androidx.datastore:datastore-preferences:1.1.0-rc01") TODO don't abuse shared prefs
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.mediarouter:mediarouter:1.7.0")
     implementation("androidx.media3:media3-exoplayer:$media3Version")
--- app/src/main/kotlin/org/akanework/gramophone/ui/fragments/settings/ContributorsSettingsFragment.kt
+++ app/src/main/kotlin/org/akanework/gramophone/ui/fragments/settings/ContributorsSettingsFragment.kt
@@ -1,19 +1,11 @@
 package org.akanework.gramophone.ui.fragments.settings
 
-import android.graphics.Camera
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Matrix
-import android.graphics.Paint
 import android.os.Bundle
-import android.text.TextPaint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import org.akanework.gramophone.BuildConfig
+import android.widget.TextView
 import org.akanework.gramophone.R
-import org.akanework.gramophone.logic.ui.spans.StaticLayoutBuilderCompat
-import org.akanework.gramophone.logic.utils.CalculationUtils.lerp
 import org.akanework.gramophone.ui.fragments.BaseFragment
 import org.akanework.gramophone.ui.fragments.BaseSettingFragment
 
@@ -26,32 +18,8 @@ class ContributorsFragment : BaseFragment(null) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val start = System.currentTimeMillis()
-        val matrix = Matrix()
-        val tp = TextPaint()
-        val camera = Camera()
-        tp.color = Color.RED
-        tp.textSize = 100f
-        tp.textAlign = Paint.Align.CENTER
-        return object : View(requireActivity()) {
-            override fun onDraw(canvas: Canvas) {
-                val time = (System.currentTimeMillis() - start).toFloat() / 1000f
-                canvas.save()
-                camera.save()
-                camera.rotate(lerp(-90f, 0f, (time % 10f) / 10f), lerp(90f, 0f, (time % 10f) / 10f), 0f)
-                matrix.reset()
-                camera.getMatrix(matrix)
-                camera.restore()
-                matrix.preTranslate(width / 2f, 50f)
-                //matrix.postTranslate(width / 2f, 0f)
-                canvas.concat(matrix)
-                StaticLayoutBuilderCompat.obtain("Gramophone\n" +
-                        "v${BuildConfig.VERSION_NAME}\nBy nift4, 123Duo3, AkaneTan, WSTxda, imjyotiraditya", tp, width)
-                    .build()
-                    .draw(canvas)
-                canvas.restore()
-                invalidate()
-            }
+        return TextView(requireActivity()).also {
+            it.text = requireContext().getString(R.string.settings_contributors_long)
         }
     }
 }
