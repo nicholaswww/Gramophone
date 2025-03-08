3fa245f24b684c1f56d9115a6d93da975c78fb8e
nift4 <nift4@protonmail.com>
2025-03-08 17:45:03 +0100

    Release 1.0.16.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         // keep support for it for a while.
         minSdk = 21
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.0.16"
+        versionCode = 19
+        versionName = "1.0.16.1"
         if (releaseType != "Release") {
             versionNameSuffix = myVersionName
         }
