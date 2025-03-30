845ce3783f702dae53fa47d724cff4bf354174fd
Nicholas <programmergeo@gmail.com>
2025-03-30 18:20:19 +0400

    Update Coil to version 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -265,7 +265,7 @@ dependencies {
     implementation("com.mikepenz:aboutlibraries:$aboutLibsVersion")
     implementation("com.google.android.material:material:1.13.0-alpha12")
     implementation("me.zhanghai.android.fastscroll:library:1.3.0")
-    implementation("io.coil-kt.coil3:coil:3.0.4")
+    implementation("io.coil-kt.coil3:coil:3.1.0")
     implementation("org.lsposed.hiddenapibypass:hiddenapibypass:6.1")
     //noinspection GradleDependency newer versions need java.nio which is api 26+
     //implementation("com.github.albfernandez:juniversalchardet:2.0.3") TODO
