6a412bac642eb2284c1f6c4f2f41dd05f55c844b
nift4 <nift4@protonmail.com>
2025-07-19 15:12:28 +0200

    update agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "8.12.0-alpha07"
+    val agpVersion = "8.12.0-rc01"
     id("com.android.application") version agpVersion apply false
 	id("com.android.library") version agpVersion apply false
 	id("com.android.test") version agpVersion apply false
