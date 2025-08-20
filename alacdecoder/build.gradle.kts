46feab75ecfdecaf936ad7f6c65fdc3df3626dfe
nift4 <nift4@protonmail.com>
2025-08-20 13:04:49 +0200

    add alacdecoder

---
+++ alacdecoder/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "org.nift4.alacdecoder"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+dependencies {
+    implementation("androidx.media3:media3-exoplayer")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 @file:Suppress("UnstableApiUsage")
 
+include(":alacdecoder")
+
+
 pluginManagement {
     repositories {
         google()
