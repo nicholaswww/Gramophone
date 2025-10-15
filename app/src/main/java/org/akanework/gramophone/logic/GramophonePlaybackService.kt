08f0c6f878604f8c3432d04c59586e25f70346c0
nift4 <nift4@protonmail.com>
2025-10-15 10:22:44 +0200

    enable mp3 index seeking by default

--- app/src/main/java/org/akanework/gramophone/logic/GramophonePlaybackService.kt
+++ app/src/main/java/org/akanework/gramophone/logic/GramophonePlaybackService.kt
@@ -353,8 +353,7 @@ class GramophonePlaybackService : MediaLibraryService(), MediaSessionService.Lis
                     .setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER),
                 GramophoneMediaSourceFactory(DefaultDataSource.Factory(this), GramophoneExtractorsFactory().also {
                     it.setConstantBitrateSeekingEnabled(true)
-                    if (prefs.getBooleanStrict("mp3_index_seeking", false))
-                        it.setMp3ExtractorFlags(Mp3Extractor.FLAG_ENABLE_INDEX_SEEKING)
+                    it.setMp3ExtractorFlags(Mp3Extractor.FLAG_ENABLE_INDEX_SEEKING)
                 }))
                 .setWakeMode(C.WAKE_MODE_LOCAL)
                 .setAudioAttributes(
